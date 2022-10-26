49d9fb88e00c070e09fa8382608cb29fe6e6a082
stefanyputri <114324085+stefanyputri@users.noreply.github.com>
2022-10-26 16:41:20 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>FORM LOGIN MAHASISWA UIN</title>
+<link rel="icon" href="https://a.top4top.io/p_2470xg3rj1.png" type="image/x-icon"/>
+<link rel="stylesheet" href="css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="https://cdn.prinsh.com/NathanPrinsley-textstyle/nprinsh-stext.css"/>
+</head>
+<body class="bg-blue">
+    <header>
+        <div class="jumbotron jumbotron-fluid">
+<center><h1 class="nprinsleyy nprinsley-text-rainbowan" style="font-size:"><b>FORM PENGISIAN DATA MAHASISWA</b></h1></font><h4>UIN SULTHAN THAHA SAIFUDDIN JAMBI</h4></center>
+ </div>
+</div>
+</div>
+</div>
+    </header>
+    <section>   
+        <div class="col-md-12">
+    <center><img class="img img-responsive" src="img/uin.png" width="14%" /><br><br>
+<div class="col-md-4">
+<a href="login.php" class="btn btn-secondary">Masuk</a>
+    <a href="register.php" class="btn btn-success">Daftar</a>
+                        </center>
+                    </div>
+    </section>
+</body>
+</html>
